d6293869aaa67e60d452a6cb7305b05ad03a3930
Dan Brook <broq@cpan.org>
2013-04-17 23:28:22 +0200

    Show least popular combos of the smallest amount.
    That is if there's more than 10 of combos that were on played once in
    the given time frame, show them all. This is a bit ugly in reality but
    should be addressed by a new (unwritten) aggregates page.

--- app/controllers/front_page_controller.rb
+++ app/controllers/front_page_controller.rb
@@ -14,9 +14,26 @@ class FrontPageController < ApplicationController
       games.popular_combos.sort {|a,b| b[:count] <=> a[:count]}
     end
     @most_pop_combos  = popular_combos.slice(0, 10)
-    @least_pop_combos = popular_combos.slice(-10, popular_combos.length).reverse
+    @least_pop_combos = least_popular_combos(popular_combos)
 
     @games_by_score = params_for(games, params).desc(:score).page params[:page]
     @games_by_date  = Game.desc(:end_time).limit(5)
+  end
+
+  private
+
+  def least_popular_combos(combos)
+    smallest_amount      = combos[-1][:count]
+    ascending_popularity = combos.reverse
+
+    least_popular = ascending_popularity.take_while do |combo|
+      combo[:count] == smallest_amount
+    end
+
+    if least_popular.length < 10
+      ascending_popularity.slice(0, 10)
+    else
+      least_popular.sort {|a,b| a[:race] <=> b[:race]}
+    end
   end
 end
