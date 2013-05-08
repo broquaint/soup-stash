5d9722fcb8eaabcd23bc3a34b3da0f08f1e095f5
Dan Brook <broq@cpan.org>
2013-05-08 14:53:57 +0200

    Have the game duration be human readable.
    Because parsing the default output is a bit fiddly.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 module GamesHelper
   def morgue_uri(game)
     host = URI.parse(game.server).host
@@ -14,5 +15,30 @@ module GamesHelper
   def server_link(game)
     uri = URI.parse(game.server)
     link_to DCSS::HOST_TO_ABBR[uri.host], uri.to_s, title: uri.host
+  end
+
+  def nice_duration(game)
+    match = game.duration.match /(?:(?<d>\d+)\s+)?(?<h>\d+):(?<m>\d+):(?<s>\d+)/
+    
+    total_time = if match
+                   (( match[:d].to_i || 0 ) * 86000) +
+                   (( match[:h].to_i || 0 ) * 3600)  +
+                   (( match[:m].to_i || 0 ) * 60 )   +
+                   match[:s].to_i
+                 else
+                   game.duration.to_i
+                 end
+    
+    humanize total_time
+  end
+
+  # Thanks to Mladen JablanoviÄfor this via http://stackoverflow.com/a/4136485
+  def humanize(secs)
+    [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{ |count, name|
+      if secs > 0
+        secs, n = secs.divmod(count)
+        "#{n.to_i} #{name}"
+      end
+    }.compact.reverse.join(', ')
   end
 end
